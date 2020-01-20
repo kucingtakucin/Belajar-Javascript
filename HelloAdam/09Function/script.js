5a001d744a31c82e4dd7741d5bd1f6372fbfc11d
AdamArthurFaizal <adam.faizal.af6@gmail.com>
2020-01-20 14:13:43 +0700

    Menambahkan materi baru tentang dasar-dasar javascript

--- HelloAdam/09Function/script.js
+++ HelloAdam/09Function/script.js
@@ -21,8 +21,46 @@ function Vtotal(a, b) {
     return total;
 }
 
-a = 4;
+a = 3;
 b = 8;
 console.log("Volume kubus A dengan sisi " + a + " adalah " + kubus_A(a));
 console.log("Volume kubus B dengan sisi " + b + " adalah " + kubus_B(b));
 console.log("Maka volume total adalah " + Vtotal(a, b));
+console.log('\n');
+
+function tambah() {
+    let hasil = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        hasil += arguments[i];
+    }
+    return hasil;
+}
+
+console.log("Hasil : " + tambah(1, 2, 3, 4, 5, 6));
+console.log('\n');
+
+function faktorial(n) {
+    var hasil = 0;
+    if (n === 1) {
+        hasil = 1;
+    } else {
+        hasil = n * faktorial(n - 1);
+    }
+    return hasil;
+}
+n = 5;
+console.log("Hasil dari " + n + "! adalah " + faktorial(n));
+console.log('\n');
+
+function fibonacci(n) {
+    var hasil = 0;
+    if (n === 1 || n === 0) {
+        hasil = 1;
+    } else {
+        hasil = fibonacci(n - 1) + fibonacci(n - 2);
+
+    }
+    return hasil;
+}
+m = 8;
+console.log("Hasil dari deret fibonacci ke-" + m + " adalah " + fibonacci(m));
---
+++ HelloAdam/10Array/script.js
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2020. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+Nama = ["Adam", "Arthur", "Faizal"];
+console.log(Nama);
+console.log(Nama.join());
+No = ["M", 3, '1', 1, '9', 0, '0', 1];
+console.log(No);
+Daftar = [123, "abc", true, fungsi()];
+console.log(Daftar);
+
+function fungsi() {
+    console.log("Ini adalah fungsi");
+}
+
+for (i = 0; i < Nama.length; i++) {
+    console.log(Nama[i]);
+}
+for (i = 0;i < No.length;i++) {
+    console.log(No[i]);
+}
+for (i = 0;i < Daftar.length;i++) {
+    console.log(Daftar[i]);
+}
